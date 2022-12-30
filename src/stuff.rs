e840d8cc865619150c689be46c87554105b740e0
jamey <jamey@jybryce.dev>
2022-12-29 18:40:11 -0600

    enum Coin and fn value_in_cents from section 6.2 of the rust book added

--- src/stuff.rs
+++ src/stuff.rs
@@ -31,6 +31,22 @@ pub enum Message {
 //     println!("BLAST OFF!");
 // }
 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 2,
+        Coin::Dime => 10,
+        Coin::Quarter => 25
+    }
+}
+
 pub fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
