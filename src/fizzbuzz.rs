e571a6fe6b8f7320da6bea9e0b22331f6a13689a
Jamey Bryce <jybryce4@gmail.com>
2023-01-01 15:07:14 -0600

    changed form of logic in fizzbuzz.rs from !() to !=

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -1,10 +1,10 @@
 pub fn fizz_buzz(n: usize) {
     let mut count = 1;
     while count <= n {
-        if count % 3 == 0 && !(count % 5 == 0) {
+        if count % 3 == 0 && count % 5 != 0 {
             println!("Fizz");
         }
-        else if count % 5 == 0 && !(count % 3 == 0) {
+        else if count % 5 == 0 && count % 3 != 0 {
             println!("Buzz");
         }
         else if count % 3 == 0 && count % 5 == 0 {
