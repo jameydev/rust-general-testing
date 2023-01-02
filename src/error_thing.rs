4e4e0b5ea4129e8bb14b03c8c1425c0f9e9562f7
Jamey Bryce <jybryce4@gmail.com>
2023-01-02 16:07:07 -0600

    made read_username_from_file example function

--- src/error_thing.rs
+++ src/error_thing.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::{fs, io};
+use std::io::{ErrorKind, Read};
 
 pub fn get_file() {
     let greeting_file_result = File::open("hello.txt");
@@ -18,3 +19,9 @@ pub fn get_file() {
     };
 }
 
+pub fn read_username_from_file() -> Result<String, io::Error> {
+    // let mut username = String::new();
+    // File::open("heck.txt")?.read_to_string(&mut username)?;
+    // Ok(username)
+    fs::read_to_string("hello.txt")
+}
