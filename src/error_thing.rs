bbda6597070af487d469ea6d741fd0fed4d2d5bd
Jamey Bryce <jybryce4@gmail.com>
2023-01-02 16:42:53 -0600

    added Guess struct for validating a Guess in some kind of guessing game and last_char_of_first_ln (guess what it does?)

--- src/error_thing.rs
+++ src/error_thing.rs
@@ -2,6 +2,23 @@ use std::fs::File;
 use std::{fs, io};
 use std::io::{ErrorKind, Read};
 
+pub struct Guess {
+    value: i32
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value)
+        }
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 pub fn get_file() {
     let greeting_file_result = File::open("hello.txt");
 
@@ -24,4 +41,8 @@ pub fn read_username_from_file() -> Result<String, io::Error> {
     // File::open("heck.txt")?.read_to_string(&mut username)?;
     // Ok(username)
     fs::read_to_string("hello.txt")
+}
+
+pub fn last_char_of_first_ln(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
