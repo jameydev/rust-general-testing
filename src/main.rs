0c73bdbf472aca0c62deb180cc34755146006c8b
Jamey Bryce <jybryce4@gmail.com>
2023-01-05 07:16:48 -0600

    major refactoring of main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,121 @@
 #![allow(unused)]
 extern crate core;
 
-use std::collections::HashMap;
-use std::io;
+use crate::aggregator::{Summary, Toot};
+use crate::generical::get_to_the_point;
 use error_thing::*;
 use fizzbuzz::*;
 use garden::vegetables::Asparagus;
+use life::*;
 use median::*;
 use rectangle::Rectangle;
+use std::collections::HashMap;
+use std::io;
 use stuff::*;
 use user::User;
-use crate::aggregator::{Summary, Toot};
-use crate::generical::get_to_the_point;
-use life::*;
 
+mod aggregator;
+mod error_thing;
+mod fizzbuzz;
 mod garden;
+mod generical;
+mod life;
+mod median;
+mod rectangle;
 mod stuff;
 mod user;
-mod rectangle;
-mod median;
-mod fizzbuzz;
-mod error_thing;
-mod generical;
-mod aggregator;
-mod life;
 
-fn old_crapp() {
-    stringy_thingy();
-    println!();
+pub mod general_testing {
+    use super::*;
+    pub fn old_crapp() {
+        super::stringy_thingy();
+        println!();
 
-    user_thing();
-    println!();
+        user_thing();
+        println!();
 
-    rectangular_thing();
-    println!();
+        rectangular_thing();
+        println!();
 
-    ip_thing();
-    println!();
+        ip_thing();
+        println!();
 
-    one_more_thing();
-    println!();
+        one_more_thing();
+        println!();
 
-    coin_thing();
-    println!();
+        coin_thing();
+        println!();
 
-    garden_thing();
-    println!();
+        garden_thing();
+        println!();
 
-    vector_thingy();
-    println!();
+        vector_thingy();
+        println!();
 
-    mapped_hashes_hash_mapping();
-    println!();
+        mapped_hashes_hash_mapping();
+        println!();
 
-    // Lol, this doesn't work
-    // median_tester();
-    // println!();
-    mode_tester();
-    println!();
+        // Lol, this doesn't work
+        // median_tester();
+        // println!();
+        mode_tester();
+        println!();
 
-    fizz_buzz(100);
-    println!();
+        fizz_buzz(100);
+        println!();
 
-    get_file();
-    println!();
-}
+        get_file();
+        println!();
+    }
 
-fn current_bs() {
-    let money = last_char_of_first_ln("show me that money, fam");
-    println!("The money is: {}", money.unwrap());
+    pub fn current_bs() {
+        let money = last_char_of_first_ln("show me that money, fam");
+        println!("The money is: {}", money.unwrap());
 
-    println!();
-    get_to_the_point();
+        println!();
+        get_to_the_point();
 
-    println!();
+        println!();
 
-    let toot = Toot {
-        username: String::from("jameydev"),
-        mastodon_instance: String::from("hachyderm.io"),
-        content: String::from("I nooted my profile image. NOOT NOOT FOR TOOTS😂"),
-        is_reply: false,
-        is_reblog: false,
-    };
+        let toot = Toot {
+            username: String::from("jameydev"),
+            mastodon_instance: String::from("hachyderm.io"),
+            content: String::from("I nooted my profile image. NOOT NOOT FOR TOOTS😂"),
+            is_reply: false,
+            is_reblog: false,
+        };
 
-    println!("1 new toot: {}", toot.summarize());
-}
+        println!("1 new toot: {}", toot.summarize());
+    }
 
-fn user_input() {
-    println!("\nWhat's your name?");
-    let mut name = String::new();
-    io::stdin().read_line(&mut name).expect("Didn't receive input");
-    let greeting = String::from("Nice to meet you");
-    println!("{}, {}!", &greeting, name.trim_end());
-}
+    pub fn user_input() {
+        println!("\nWhat's your name?");
+        let mut name = String::new();
+        io::stdin()
+            .read_line(&mut name)
+            .expect("Didn't receive input");
+        let greeting = String::from("Nice to meet you");
+        println!("{}, {}!", &greeting, name.trim_end());
+    }
 
-fn its_lifetimes_jim() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+    pub fn its_lifetimes_jim() {
+        let string1 = String::from("abcd");
+        let string2 = "xyz";
 
-    let result = longest(string1.as_str(), string2);
-    println!("\nThe longest string is {}", result);
+        let result = longest(string1.as_str(), string2);
+        println!("\nThe longest string is {}", result);
 
-    some_text();
-    println!();
+        some_text();
+        println!();
 
-    let the_longo = longest_with_an_announcement("ballin", "shot callin", "Yeet!");
-    println!("Longest string = {}", the_longo);
+        let the_longo = longest_with_an_announcement("ballin", "shot callin", "Yeet!");
+        println!("Longest string = {}", the_longo);
+    }
+
 }
 
 fn main() {
+    use general_testing::*;
     old_crapp();
     current_bs();
     user_input();
@@ -123,12 +130,12 @@ mod tests {
     fn lrgr_rect_can_hold_smaller() {
         let larger = Rectangle {
             width: 8,
-            height: 7
+            height: 7,
         };
 
         let smaller = Rectangle {
             width: 5,
-            height: 1
+            height: 1,
         };
 
         assert!(larger.can_contain(&smaller));
@@ -138,12 +145,12 @@ mod tests {
     fn smaller_rect_cannot_hold_lrger() {
         let larger = Rectangle {
             width: 8,
-            height: 7
+            height: 7,
         };
 
         let smaller = Rectangle {
             width: 5,
-            height: 1
+            height: 1,
         };
 
         assert!(!smaller.can_contain(&larger));
@@ -155,18 +162,3 @@ mod tests {
         Guess::new(200);
     }
 }
-
-// lame way
-// fn area(width: u32, height: u32) -> u32 {
-//     width * height
-// }
-
-// better way with tuples
-// fn area(dimensions: (u32, u32)) -> u32 {
-//     dimensions.0 * dimensions.1
-// }
-
-// Better version before struct w/ area method
-// fn area(rectangle: &Rectangle) -> u32 {
-//     rectangle.width * rectangle.height
-// }
