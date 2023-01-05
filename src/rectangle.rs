4295e8f8584b39c792a77b385a28c808411f09ee
Jamey Bryce <jybryce4@gmail.com>
2023-01-05 17:50:49 -0600

    dont even remember what changed

--- src/rectangle.rs
+++ src/rectangle.rs
@@ -1,14 +1,14 @@
 #[derive(Debug)]
 pub struct Rectangle {
     pub width: u32,
-    pub height: u32
+    pub height: u32,
 }
 
 impl Rectangle {
     pub fn square(size: u32) -> Self {
         Self {
             width: size,
-            height: size
+            height: size,
         }
     }
 
