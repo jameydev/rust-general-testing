a947a480a63633fd4081210b8b389ff20cfe811b
Jamey Bryce <jybryce4@gmail.com>
2023-01-06 23:26:24 -0600

    mutable closure example added

--- src/closed.rs
+++ src/closed.rs
@@ -1,4 +1,10 @@
 pub fn closure_examples() {
+    borrow_clsr();
+    println!();
+    mut_clsr();
+}
+
+fn borrow_clsr() {
     let list = vec![1, 2, 3];
     println!("Before defining closure: {:?}", list);
 
@@ -7,4 +13,14 @@ pub fn closure_examples() {
     println!("Before calling closure: {:?}", list);
     only_borrows();
     println!("After calling closure {:?}", list);
+}
+
+fn mut_clsr() {
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", list);
 }
